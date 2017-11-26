4cbc615058501e2d29415d9ba19a17f88774db5a
Matthijs Bossier <mebossie@student.avans.nl>
2017-11-26 15:21:21 +0100

    recipe toegevoegd in service en model

---
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Recipe } from '../../../models//recipe.model';
+import { RecipeService} from '../../../services/recipe.service';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit(): void {
+    this.recipeService.getRecipes()
+    .then(recipes => this.recipes = recipes)
+    .catch(error => console.log(error));
+  }
+
+}

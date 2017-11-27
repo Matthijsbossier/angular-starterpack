8ad5d3c585abd8abc44ef66cd9f1f97439e06a69
Bram van de Griend <shep.ukn@gmail.com>
2017-11-27 13:42:48 +0100

    Removed user.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,33 @@
-import { UserService } from './services/user.service';
+// Services.
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule, HttpClient} from '@angular/common/http';
+
+// Router.
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
+// Components.
 import { HeaderComponent } from './components/header/header.component';
-import { RecipesComponent } from "./components/recipes/recipes.component";
-import { ShoppingListService } from "./components/shopping-list/shopping-list.services";
 import { DropdownDirective } from "./shared/dropdown.directive";
+
+import { RecipesComponent } from "./components/recipes/recipes.component";
 import { RecipeItemComponent } from "./components/recipes/recipe-list/recipe-item/recipe-item.component";
 import { RecipeDetailComponent } from "./components/recipes/recipe-detail/recipe-detail.component";
 import { RecipeListComponent } from "./components/recipes/recipe-list/recipe-list.component";
+import { RecipeService } from "./services/recipe.service";
+
+import { ShoppingListService } from "./components/shopping-list/shopping-list.services";
 import { ShoppingListComponent } from "./components/shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./components/shopping-list/shopping-edit/shopping-edit.component";
-import { RecipeService } from "./services/recipe.service";
-import { HttpClientModule, HttpClient} from '@angular/common/http';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
@@ -44,10 +47,10 @@ import { HttpClientModule, HttpClient} from '@angular/common/http';
     HttpClientModule
   ],
   providers: [
-    //UserService,
     RecipeService,
     ShoppingListService
   ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
